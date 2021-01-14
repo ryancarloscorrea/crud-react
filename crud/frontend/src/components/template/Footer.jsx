d88ead32ba5b1cc7e2280a61a9eb43f53016b28e
ryancarloscorrea <ryancarlos38@gmail.com>
2021-01-13 20:24:02 -0400

    react project

---
+++ crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heart"></i> por 
+            <strong> Ryan Corrêa</strong>
+        </span>
+    </footer>
